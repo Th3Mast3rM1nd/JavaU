73d12b5dfcd755a6ffa765b2c8ec65ceffa8b50d
mast3rm1nd <mast3r.b10ta@protonmail.com>
2024-02-01 21:27:48 +0100

    new Files

--- src/ScannerInputOutput.java
+++ src/ScannerInputOutput.java
@@ -1,16 +1,30 @@
+import java.util.Scanner;
+
 public class ScannerInputOutput {
     public static void main(String[] args) {
         int currentYear = 2022;
-        System.out.println(getInputFromConsole(currentYear));
-        System.out.println(getInputFromScanner(currentYear));
+        try {
+            System.out.println(getInputFromConsole(currentYear));
+        }catch (NullPointerException e) {
+            System.out.println(getInputFromScanner(currentYear));
+        }
+
+
     }
     public static String getInputFromConsole(int currentYear) {
         // runs only on Terminal
-        String name = System.console().readLine("Hi, What's Your Name?");
-        return name;
-
-    }
+            String name = System.console().readLine("Hi, What's Your Name? ");
+            String dateOfbirth = System.console().readLine("Whats Your Age? ");
+            int age = currentYear - Integer.parseInt(dateOfbirth);
+            return "So you are" + age + "years old";
+        }
     public static String getInputFromScanner(int currentYear) {
-        return "";
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Whats Your name ");
+        String name = scanner.nextLine();
+        System.out.println("Whats year were you born");
+        String dateOfbirth = scanner.nextLine();
+        int age = currentYear - Integer.parseInt(dateOfbirth);
+        return "so you are " + age + "Old";
     }
 }
